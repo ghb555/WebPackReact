bc8eec329b0ff40ce6ad0a411eac8b40929bf218
Andrii Burov <andriiburov@Andriis-MacBook-Pro.local>
2020-04-04 12:11:45 -0700

    fix broken build

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom";
 import React from "react";
-import Form from "./js/components/Form/Form";
+import Form from "./components/Form/Form";
 
 ReactDOM.render(<Form />, document.getElementById("container"));
